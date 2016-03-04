188340ac0c2729fed70d46806133d29ab24ff699
edve98 <edve98@gmail.com>
2016-03-05 01:02:50 +0200

    added basic death screen

---
+++ Assets/Scripts/Menu scripts/LoadLevel.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class LoadLevel : MonoBehaviour {
+
+	public string sceneName;
+
+	public void load () {
+		SceneManager.LoadScene (sceneName);
+	}
+}
