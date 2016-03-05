28f2422c4aeb557c2ef70620212dc6a7a2da8e66
AinorasLaptop <AinoraZ@gmail.com>
2016-03-05 22:31:22 +0200

    DeathScriptas

---
+++ Assets/Scripts/DeathScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeathScript : MonoBehaviour {
+
+    public GameObject death;
+    PlayerControls player;
+
+	// Use this for initialization
+	void Start () {
+        player = GetComponent<PlayerControls>();
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        if (player.timeLeft == 0)
+        {
+            death.SetActive(true);
+            Time.timeScale = 0.000001f;
+            Destroy(GameObject.FindGameObjectWithTag("Player"));
+        }
+        if (player.livesLeft == 0)
+        {
+            death.SetActive(true);
+            Time.timeScale = 0.000001f;
+            Destroy(GameObject.FindGameObjectWithTag("Player"));
+        }
+	}
+}
