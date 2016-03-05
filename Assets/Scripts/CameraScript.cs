8b2e201c0721a3078419c42d3899eea1f49b8d05
edve98 <edve98@gmail.com>
2016-03-05 10:56:51 +0200

    merge

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+
+	public GameObject laser;
+	public BoxCollider laserTrigger;
+	public int laserLenght = 10;
+	public GameObject fakinAtsavra;
+	public float spinTime;
+	public int speed;
+
+	float cooldown;
+
+	void Start () {
+		laser.transform.position.Set (0, 0, laserLenght / 2);
+		laserTrigger.size.Set(0.5f, 0.5f, laserLenght);
+		fakinAtsavra.transform.position.Set (0, 0, -laserLenght);
+
+		cooldown = spinTime;
+	}
+
+	public bool decreasing = true;
+	void Update () {
+		if (cooldown <= 0){
+			decreasing = !decreasing;
+			cooldown = spinTime;
+		}
+		cooldown -= Time.deltaTime;
+
+		if(decreasing) gameObject.transform.Rotate (new Vector3(0, speed * Time.deltaTime * -1, 0));
+		else gameObject.transform.Rotate (new Vector3(0, speed * Time.deltaTime, 0));
+	}
+}
---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Laser : MonoBehaviour {
+
+	void OnTriggerEnter(Collider col){
+		if (col.tag == "Player") {
+			GameObject[] Enemies = GameObject.FindGameObjectsWithTag ("EnemyTagThingy");
+			for(int i=0; i < Enemies.Length; i++){
+				//Enemies [i].GetComponent<RobotAI> ().alerted = true;
+			}
+		}
+	}
+}
