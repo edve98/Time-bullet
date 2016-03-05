24e01fcdd8080abb62797dc3cc536ac2cdc09a3f
AinorasLaptop <AinoraZ@gmail.com>
2016-03-05 19:22:08 +0200

    AndroidControlStart

---
+++ Assets/AndroidControler.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class AndroidControler : MonoBehaviour, TDragHandler, IPointerUpHandler, IPointerDownHandler {
+
+    private Image bgImg;
+    private Image joysticImg;
+    private Vector3 inputVector;
+
+    public virtual void OnDrag(PointerEventData ped)
+    {
+        Vector2 pos;
+        if()
+    }
+    public virtual void OnPointerDown(PointerEventData ped)
+    {
+        OnDrag(ped);
+    }
+    public virtual void OnPointerUp(PointerEventData ped)
+    {
+
+    }
+
+    // Use this for initialization
+    void Start () {
+        bgImg = GetComponent<Image>();
+        joysticImg = transform.GetChild(0).GetComponent<Image>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
